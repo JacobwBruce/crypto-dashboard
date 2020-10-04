1b97db55e53b3e0d514f84e0c1a54105e23d66c0
Jacob Bruce <brucejac@sheridancollege.ca>
2020-10-04 09:43:50 -0400

    Settings page, localStorage

--- src/App/AppProvider.jsx
+++ src/App/AppProvider.jsx
@@ -6,12 +6,33 @@ export class AppProvider extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            page: 'Settings',
+            page: 'Dashboard',
+            ...this.savedSettings(),
             setPage: this.setPage,
+            confirmFavorites: this.confirmFavorites,
         };
     }
 
     setPage = (page) => this.setState({ page });
+
+    savedSettings() {
+        const cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash'));
+        if (!cryptoDashData) return { page: 'Settings', firstVisit: true };
+        return {};
+    }
+
+    confirmFavorites = () => {
+        this.setState({
+            firstVisit: false,
+            page: 'Dashboard',
+        });
+        localStorage.setItem(
+            'cryptoDash',
+            JSON.stringify({
+                test: 'hello',
+            })
+        );
+    };
 
     render() {
         return <AppContext.Provider value={this.state}>{this.props.children}</AppContext.Provider>;
