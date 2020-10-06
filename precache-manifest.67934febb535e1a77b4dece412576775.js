3269010576cd33e8276b85181ace23d6e8bb724a
Jacob Bruce <brucejac@sheridancollege.ca>
2020-10-06 13:04:17 -0400

    Updates

---
+++ precache-manifest.67934febb535e1a77b4dece412576775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c26176edcc2023cbebf6fc7f99f5be",
+    "url": "/crypto-dashboard/index.html"
+  },
+  {
+    "revision": "bcade864955e76d0d2fc",
+    "url": "/crypto-dashboard/static/css/main.d85c7f9f.chunk.css"
+  },
+  {
+    "revision": "5a87f8cec4298913f78f",
+    "url": "/crypto-dashboard/static/js/2.b7586353.chunk.js"
+  },
+  {
+    "revision": "360978b69b2d355b7f6ed150ede4e119",
+    "url": "/crypto-dashboard/static/js/2.b7586353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcade864955e76d0d2fc",
+    "url": "/crypto-dashboard/static/js/main.da629e9e.chunk.js"
+  },
+  {
+    "revision": "c03be437a17d41ffbde5",
+    "url": "/crypto-dashboard/static/js/runtime-main.7a9bc886.js"
+  }
+]);
